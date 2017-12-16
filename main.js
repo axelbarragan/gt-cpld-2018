48d9471fca5d0f335e59e190751f4d00920cc6c3
Axel Barragan <axelbarragan@gmail.com>
2017-12-15 18:11:26 -0600

    Cambios

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
 $(function () { // wait for document ready
 
-   $('.owl-carousel').owlCarousel();
-   console.log("D");
+ var owl = $('.owl-carousel');
+ owl.owlCarousel({
+  autoplay:true,
+  loop: true,
+});
+ owl.on('changed.owl.carousel', function(event) {
+  jeje();
+ })
 
-   TweenMax.from( $('#orale'), 1.5,
-    {css:{scale:0.55, opacity:0, rotation: 10}, 
+ function jeje() {
+   TweenMax.from( $('.orale'), 1.5,
+    {css:{scale:0.5, opacity:0, rotation: 10}, 
     ease:Quad.easeInOut
   });
+ }
 
 
-var x = $(".one h1").offset();
-var one = $(".one").width();
-var para = $(".one h1").width();
-var right = one - (x.left + para);
-var twoOffset = $(".two h1").offset();
-var twoLeftOffset = twoOffset.left - one;
-var firstLine = twoLeftOffset + right;
-var leftPos = para + x.left;
 
-/*$(".horizontal-line").css({"top": x.top, "left": leftPos});*/
+ var x = $(".one h1").offset();
+ var one = $(".one").width();
+ var para = $(".one h1").width();
+ var right = one - (x.left + para);
+ var twoOffset = $(".two h1").offset();
+ var twoLeftOffset = twoOffset.left - one;
+ var firstLine = twoLeftOffset + right;
+ var leftPos = para + x.left;
+
+ /*$(".horizontal-line").css({"top": x.top, "left": leftPos});*/
 // init controller
 var controller = new ScrollMagic.Controller();
 
@@ -26,25 +35,25 @@ var controller = new ScrollMagic.Controller();
 
     // define movement of panels
     var wipeAnimation = new TimelineMax()
-      .to("#slideContainer", 1,   {x: "-75%"})
+    .to("#slideContainer", 1,   {x: "-75%"})
 
     // create scene to pin and link animation
     new ScrollMagic.Scene({
-        triggerElement: "#pinContainer",
-        triggerHook: "onLeave",
-        duration: "200%"
-      })
-      .setPin("#pinContainer")
-      .setTween(wipeAnimation)
+      triggerElement: "#pinContainer",
+      triggerHook: "onLeave",
+      duration: "200%"
+    })
+    .setPin("#pinContainer")
+    .setTween(wipeAnimation)
       ////.addIndicators()
       .addTo(controller);
 
 
-   var horizontal = new ScrollMagic.Scene({
+      var horizontal = new ScrollMagic.Scene({
         offset: 50,
         duration: 300,
        // reverse: false
       }).setTween(".horizontal-line", {width: firstLine}) // trigger a TweenMax.to tween
         // .addIndicators()
         .addTo(controller);
-              });
+      });
